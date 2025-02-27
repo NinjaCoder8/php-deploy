614c65c00e556d0c0bd6638864372b9850412def
Charbel <charbel.dawoud@gmail.com>
2025-02-27 19:14:14 +0100

    api get articles

---
+++ conection/connection.php
@@ -0,0 +1,12 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$password = ""; //s$f2o5;;
+$db_name = "articles_db";
+
+$mysqli = new mysqli($host, $user, $password, $db_name);
+
+//check for errors 
+
+?>
--- getArticles.php
+++ getArticles.php
@@ -1,5 +1,29 @@
 <?php
 
-echo "Hello World New!";
+include("conection/connection.php");
+
+if(isset($_GET["id"])){
+    $id = $_GET["id"];
+}else{
+    $id = -1;
+}
+
+if($id == -1){
+    $query = $mysqli->prepare("SELECT * FROM articles");
+}else{
+    $query = $mysqli->prepare("SELECT * FROM articles WHERE id = ?");
+    $query->bind_param("i", $id);
+}
+
+$query->execute();
+
+$array = $query->get_result();
+
+$response = [];
+while($article = $array->fetch_assoc()){
+    $response[] = $article;
+}
+
+echo json_encode($response);
 
 ?>
